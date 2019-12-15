c224f2a222eec07f71d3dc5314d08335e8c75c56
belloosagienoah <osagienoah555@gmail.com>
2019-12-15 01:47:37 -1100

    Commit Changes

---
+++ script.js
@@ -0,0 +1,43 @@
+// incrementor and decrementor
+
+// Post-increment operator
+var number = 5;
+number++; // 5
+console.log(number); // 6
+
+// Number is now 6
+number--; // 6
+console.log(number); // 5
+
+// Number is now 5
+number++; // 5
+console.log(number) // 6
+
+// Pre-increment operator
+var number = 5;
+++number; // 6
+console.log(number)
+
+// Number is now 6
+--number; // 5
+console.log(number); // 5
+
+// Number is now 5
+number--;
+console.log(number); // 4
+
+// From the examples:
+// What is pre-increment operator (a = ++x)
+// What is post-increment operator (a = x++)
+
+/* 
+Pre-increment operator: A pre-increment operator is used to increment the value of a variable before using 
+it in a expression. In the Pre-Increment, value is first incremented and then used inside the expression.
+Syntax: a = ++x;
+*/
+
+/*
+Post-increment operator: A post-increment operator is used to increment the value of variable after executing expression 
+completely in which post increment is used. In the Post-Increment, value is first used in a expression and then incremented.
+Syntax: a = x++;
+*/
